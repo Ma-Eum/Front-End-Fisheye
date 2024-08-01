5576f518d76134cd285869c1623bf6aa5b07b5db
Ma-Eum <cl.gachelin@gmail.com>
2024-08-01 03:08:50 +0200

    rajout de l'affichage de la localisation, la tagline, et le prix avec modification du CSS en consequence

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,38 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
-
+    const { name, portrait, city, country, tagline, price } = data;
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement('article');
+        
+        const img = document.createElement('img');
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", `${name}`);
+        img.setAttribute("aria-label", `${name}`);
+        
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+
+        const location = document.createElement('p');
+        location.textContent = `${city}, ${country}`;
+        location.classList.add('location');
+
+        const taglineEl = document.createElement('p');
+        taglineEl.textContent = tagline;
+        taglineEl.classList.add('tagline');
+
+        const priceEl = document.createElement('p');
+        priceEl.textContent = `${price}€/jour`;
+        priceEl.classList.add('price');
+
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(location);
+        article.appendChild(taglineEl);
+        article.appendChild(priceEl);
+
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+
+    return { name, picture, getUserCardDOM };
 }

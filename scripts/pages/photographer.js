cccdb7962dc03db5489c0f192d36b1fec9c0647b
Ma-Eum <cl.gachelin@gmail.com>
2024-08-01 02:51:54 +0200

    Mise à jour du template des photographes  pour inclure des informations supplémentaires

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,40 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+function photographerTemplate(data) {
+    const { name, portrait, city, country, tagline, price } = data;
+    const picture = `assets/photographers/${portrait}`;
+
+    function getUserCardDOM() {
+        const article = document.createElement('article');
+        
+        const img = document.createElement('img');
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", `${name}`);
+        img.setAttribute("aria-label", `${name}`);
+        
+        const h2 = document.createElement('h2');
+        h2.textContent = name;
+
+        const location = document.createElement('p');
+        location.textContent = `${city}, ${country}`;
+        location.classList.add('location');
+
+        const taglineEl = document.createElement('p');
+        taglineEl.textContent = tagline;
+        taglineEl.classList.add('tagline');
+
+        const priceEl = document.createElement('p');
+        priceEl.textContent = `${price}€/jour`;
+        priceEl.classList.add('price');
+
+        article.appendChild(img);
+        article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(taglineEl);
+        article.appendChild(priceEl);
+
+        return (article);
+    }
+
+    return { name, picture, getUserCardDOM };
+}
